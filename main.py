63d5ea0f64ffc99762630cdd8d4986c4b2be6aba
Andrew Wang <me@andrewwang.ca>
2019-02-28 02:00:27 -0500

    Remove use_context

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("779535573:AAH-mPuQLTPn5XHU_hQ-lMd53YKi6_1zZCY", use_context=True)
+    updater = Updater("779535573:AAH-mPuQLTPn5XHU_hQ-lMd53YKi6_1zZCY")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
