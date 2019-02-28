a8d12a766fc41d5c3b192fb272e54f1526268b47
Andrew Wang <me@andrewwang.ca>
2019-02-28 02:13:05 -0500

    Bind port

--- main.py
+++ main.py
@@ -81,4 +81,22 @@ def main():
 
 
 if __name__ == '__main__':
+    import socket
+    import sys
+    import os
+
+    HOST = ''
+    PORT = int(os.environ.get("PORT", 8080))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.bind((HOST, PORT))
+
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+
+    print('Socket bind complete')
+
     main()
