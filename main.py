d2f8018eec93693f7bf3289f72d869ade8135e27
Andrew Wang <me@andrewwang.ca>
2019-02-28 03:33:29 -0500

    Add file size limit (1 MB)

--- main.py
+++ main.py
@@ -30,12 +30,33 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+
+def file_size(file_path):
+    """
+    this function will return the file size
+    """
+    if os.path.isfile(file_path):
+        file_info = os.stat(file_path)
+        return convert_bytes(file_info.st_size)
+
+
 def get_pic_list(path):
     result = []
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-                result.append(os.path.join(root, file))
+                file_path = os.path.join(root, file)
+                if "KB" in file_size(file_path):
+                    result.append(file_path)
     return result
 
 
