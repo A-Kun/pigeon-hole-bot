5b88d5d32f496fc92cdb3c57bf109907a46b4399
Andrew Wang <me@andrewwang.ca>
2019-06-30 02:01:19 -0400

    Clean up

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
-#
-# THIS EXAMPLE HAS BEEN UPDATED TO WORK WITH THE BETA VERSION 12 OF PYTHON-TELEGRAM-BOT.
-# If you're still using version 11.1.0, please see the examples at
-# https://github.com/python-telegram-bot/python-telegram-bot/tree/v11.1.0/examples
-
-"""
-Simple Bot to reply to Telegram messages.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 import os
 import random
@@ -84,8 +65,7 @@ def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Help!')
 
 
-def echo(bot, update):
-    """Echo the user message."""
+def setu(bot, update):
     if (random.random() < 0.5):
         pic_list = get_pic_list("../pigeon-hole-bot-media")
         if len(pic_list) > 0:
@@ -103,9 +83,6 @@ def error(bot, update):
 
 def main():
     """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     with open('.env') as env:
         updater = Updater(env.read().strip())
 
@@ -115,10 +92,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("色图", echo))
-
-    # on noncommand i.e message - echo the message on Telegram
-    # dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler("色图", setu))
 
     # log all errors
     dp.add_error_handler(error)
